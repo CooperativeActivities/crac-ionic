d3e6e3e91d916b035a279b582c684cb0d4d4bd7b
fabidop <fabidop@gmail.com>
2016-04-26 19:01:54 +0200

    -- testviews by dopler

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('cameraTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+         
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('registrierungCtrl', function($scope) {
+
+})
+ 
