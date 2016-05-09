d9c2b44b52f5077757d89ce7aa883bcb0282b815
Danjelous <daniel.brunner@live.at>
2016-05-09 10:15:28 +0200

    Initial touch src

--- www/js/routes.js
+++ www/js/routes.js
@@ -7,8 +7,8 @@ angular.module('app.routes', [])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-    
-  
+
+
 
       .state('tabsController.neuigkeiten', {
     url: '/footer_news',
@@ -96,6 +96,6 @@ angular.module('app.routes', [])
 
 $urlRouterProvider.otherwise('/login')
 
-  
+
 
 });
