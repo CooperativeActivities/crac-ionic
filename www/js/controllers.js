5dbd62cf14a6742cea4e5cd39dfdff48c01b1092
fabidop <fabidop@gmail.com>
2016-05-18 16:30:37 +0200

    -- REBUILD!!! - dopl

---
+++ www/js/controllers.js
@@ -0,0 +1,46 @@
+angular.module('app.controllers', [])
+  
+.controller('neuigkeitenCtrl', function($scope) {
+
+})
+   
+.controller('entdeckungsreiseCtrl', function($scope) {
+
+})
+   
+.controller('benachrichtigungenCtrl', function($scope) {
+
+})
+   
+.controller('aufgabenCtrl', function($scope) {
+
+})
+         
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('registrierungCtrl', function($scope) {
+
+})
+   
+.controller('organisationCtrl', function($scope) {
+
+})
+   
+.controller('meinProfilCtrl', function($scope) {
+
+})
+   
+.controller('projekteCtrl', function($scope) {
+
+})
+   
+.controller('gelSchteAufgabenCtrl', function($scope) {
+
+})
+   
+.controller('einstellungenCtrl', function($scope) {
+
+})
+ 
