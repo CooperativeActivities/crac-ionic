2dbdec4a847dbd46a08d8acaed6b82864cceb4f5
fabidop <fabidop@gmail.com>
2016-06-08 10:45:42 +0200

    -- edited logos & android nav bar to bottom- dopl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,151 +1,1 @@
-angular.module('app.controllers', [])
-
-.controller('neuigkeitenCtrl', function($scope, $http) {
-    $http.get('data/news_dummy.json').success(function(data) {
-        $scope.newsList = data;
-    });
-})
-
-.controller('entdeckungsreiseCtrl', function($scope) {
-
-})
-
-.controller('benachrichtigungenCtrl', function($scope) {
-
-})
-
-.controller('aufgabenCtrl', function($scope) {
-
-})
-
-.controller('loginCtrl', function($scope, $http, $location, UserDataService, $ionicSideMenuDelegate) {
-	$ionicSideMenuDelegate.canDragContent(false);
-
-	$scope.checkLogin = function(email, password) {
-
-		console.log(email);
-		console.log(password);
-		// Check if demo-user (frontend@test.at; frontendKey)
-		if(email === "frontend@test.at" &&
-			password === "frontendKey")
-		{
-
-			// Get local JSON
-			$http.get('data/login_dummy.json').success(function(data) {
-				$scope.login = data;
-			});
-			console.log("ok");
-
-			$location.path("/tabs/footer_news");
-		
-		}
-		else {
-			console.log("error");
-		}
-	}
-
-	// Get the user from a authentication service before (basic auth)
-	/*
-	UserDataService.getUserById(2).then(
-		function($res){
-			console.log($res.data);
-		},
-		function($error){
-			console.log($error);
-		}
-	);
-	*/
-
-
-})
-
-.controller('registrierungCtrl', function($scope) {
-
-})
-
-.controller('organisationCtrl', function($scope) {
-
-})
-
-
-
-.controller('meinProfilCtrl', function($scope, $http) {
-
-	// Get local JSON
-	$http.get('data/user_dummy.json').success(function(data) {
-   	    $scope.user = data;
-	});
-
-})
-
-.controller('projekteCtrl', function($scope, $http) {
-
-        $http.get('data/project_tasklist_dummy.json').success(function(data) {
-            $scope.projectList = data;
-        });
-})
-
-.controller('gelSchteAufgabenCtrl', function($scope) {
-
-})
-
-.controller('einstellungenCtrl', function($scope) {
-
-})
-
-.controller('LoginCtrl', function ($window, $scope, $location, $route, AuthenticationService, FlashService) {
-
-
-
-
-
-	/*
-	 * ---------------------------
-    *      LOGIN SERVER
-	 * ---------------------------
-
-
-    $scope.logout = function () {
-        // reset login status
-        AuthenticationService.ClearCredentials();
-        AuthenticationService.Logout();
-        $location.path('/user');
-        $window.location.reload();
-    };
-
-    $scope.loggedIn = $scope.globals.currentUser;
-
-
-    $scope.login = function login(){
-        console.log("in login");
-        console.log($scope);
-
-        $scope.dataLoading = true;
-
-        //debugger;
-        AuthenticationService.Login($scope.username, $scope.password, function (response) {
-            if (response.success) {
-                console.log("in login - AuthenticationService.Login == successed");
-                console.log("Response: "+response);
-                //AuthenticationService.SetCredentials($scope.username, $scope.password);
-                $scope.loggedIn = true;
-
-                FlashService.Success("Eingeloggt!");
-
-                if($scope.globals.currentUser.isAdmin)
-                    $location.path('/user');
-                else
-                    $location.path('/failed');
-
-                $window.location.reload();
-            } else {
-                console.log("in login - AuthenticationService.Login == failed");
-                FlashService.Error(response.message);
-                $scope.dataLoading = false;
-                $scope.loggedIn = false;
-            }
-        });
-        console.log("in login - AuthenticationService.Login == passed");
-    };
-	 */
-})
+angular.module('app.controllers', []).config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {	$ionicConfigProvider.tabs.position('bottom');}).controller('neuigkeitenCtrl', function($scope, $http) {    $http.get('data/news_dummy.json').success(function(data) {        $scope.newsList = data;    });}).controller('entdeckungsreiseCtrl', function($scope) {}).controller('benachrichtigungenCtrl', function($scope) {}).controller('aufgabenCtrl', function($scope) {}).controller('loginCtrl', function($scope, $http, $location, UserDataService, $ionicSideMenuDelegate) {	$ionicSideMenuDelegate.canDragContent(false);	$scope.checkLogin = function(email, password) {		console.log(email);		console.log(password);		// Check if demo-user (frontend@test.at; frontendKey)		if(email === "frontend@test.at" &&			password === "frontendKey")		{			// Get local JSON			$http.get('data/login_dummy.json').success(function(data) {				$scope.login = data;			});			console.log("ok");			$location.path("/tabs/footer_news");				}		else {			console.log("error");		}	}	// Get the user from a authentication service before (basic auth)	/*	UserDataService.getUserById(2).then(		function($res){			console.log($res.data);		},		function($error){			console.log($error);		}	);	*/}).controller('registrierungCtrl', function($scope) {}).controller('organisationCtrl', function($scope) {}).controller('meinProfilCtrl', function($scope, $http) {	// Get local JSON	$http.get('data/user_dummy.json').success(function(data) {   	    $scope.user = data;	});}).controller('projekteCtrl', function($scope, $http) {        $http.get('data/project_tasklist_dummy.json').success(function(data) {            $scope.projectList = data;        });}).controller('gelSchteAufgabenCtrl', function($scope) {}).controller('einstellungenCtrl', function($scope) {}).controller('LoginCtrl', function ($window, $scope, $location, $route, AuthenticationService, FlashService) {	/*	 * ---------------------------    *      LOGIN SERVER	 * ---------------------------    $scope.logout = function () {        // reset login status        AuthenticationService.ClearCredentials();        AuthenticationService.Logout();        $location.path('/user');        $window.location.reload();    };    $scope.loggedIn = $scope.globals.currentUser;    $scope.login = function login(){        console.log("in login");        console.log($scope);        $scope.dataLoading = true;        //debugger;        AuthenticationService.Login($scope.username, $scope.password, function (response) {            if (response.success) {                console.log("in login - AuthenticationService.Login == successed");                console.log("Response: "+response);                //AuthenticationService.SetCredentials($scope.username, $scope.password);                $scope.loggedIn = true;                FlashService.Success("Eingeloggt!");                if($scope.globals.currentUser.isAdmin)                    $location.path('/user');                else                    $location.path('/failed');                $window.location.reload();            } else {                console.log("in login - AuthenticationService.Login == failed");                FlashService.Error(response.message);                $scope.dataLoading = false;                $scope.loggedIn = false;            }        });        console.log("in login - AuthenticationService.Login == passed");    };	 */})
