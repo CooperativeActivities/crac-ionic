48eb1daf2a55ba392924a64612b2e77fe81e1da1
fabidop <fabidop@gmail.com>
2016-06-20 14:24:52 +0200

    -- logout & deleteAcc --dopl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -240,14 +240,70 @@ angular.module('app.controllers', [])
 
 
 
-.controller('meinProfilCtrl', function($scope, $http) {
+.controller('meinProfilCtrl', function($scope, $http, $ionicModal) {
 
 	// Get local JSON
 	$http.get('data/user_dummy.json').success(function(data) {
    	    $scope.user = data;
 	});
 
+	// Picture Change expand modal
+	$ionicModal.fromTemplateUrl('profileimage-modal.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
+	$scope.openModal = function() {
+		$scope.modal.show();
+	};
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+	};
+	// Cleanup the modal when we're done with it!
+	$scope.$on('$destroy', function() {
+		$scope.modal.remove();
+	});
+	// Execute action on hide modal
+	$scope.$on('modal.hidden', function() {
+		// Execute action
+	});
+	// Execute action on remove modal
+	$scope.$on('modal.removed', function() {
+		// Execute action
+	});
+
 })
+
+.controller('logoutCtrl', function($scope, $ionicModal) {
+
+		// Logout Modal
+		$ionicModal.fromTemplateUrl('logout-modal.html', {
+			scope: $scope,
+			animation: 'slide-in-up'
+		}).then(function(modal) {
+			$scope.modal = modal;
+		});
+		$scope.openModal = function() {
+			$scope.modal.show();
+		};
+		$scope.closeModal = function() {
+			$scope.modal.hide();
+		};
+		// Cleanup the modal when we're done with it!
+		$scope.$on('$destroy', function() {
+			$scope.modal.remove();
+		});
+		// Execute action on hide modal
+		$scope.$on('modal.hidden', function() {
+			// Execute action
+		});
+		// Execute action on remove modal
+		$scope.$on('modal.removed', function() {
+			// Execute action
+		});
+})
+
 
 .controller('projektCtrl', function($scope, $http, $stateParams) {
 
